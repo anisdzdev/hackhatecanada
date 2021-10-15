b6c9a92a3e2bcc7bcac1dc621944b78a5e3afbc1
meroz <meroz@DESKTOP-G2U7FR4>
2021-10-15 17:31:08 -0400

    crawler skeleton

--- webpage-parser/hackhatespiders/hackhatespiders/spiders/hackhate_spider.py
+++ webpage-parser/hackhatespiders/hackhatespiders/spiders/hackhate_spider.py
@@ -1,9 +1,29 @@
 import scrapy
-from hackhatespiders.url_picker import get_new_url
+import json
 
 class HackHateSpider(scrapy.Spider):
     name = "hackhate"
-    start_urls = get_new_url(False)
+    num_urls = 10
+    
+    def __init__(self, num_urls=10):
+        self.num_urls = num_urls
+
+    def start_requests(self):
+        with open('test.json', 'r+', encoding='utf8') as f:
+            data = json.load(f)
+            counter = 0
+            for index, row in enumerate(data):
+                if row['isUsed'] == False:
+                    returnedUrl = row['url']
+                    data[index]['isUsed'] = True
+                    ++counter
+                    
+                    if counter == self.num_urls:
+                        f.seek(0)
+                        json.dump(data, f, indent=4)
+                        f.truncate()
+                        break
+                    yield scrapy.Request(returnedUrl, callback=self.parse)
 
     def parse(self, response):
         print(response)
