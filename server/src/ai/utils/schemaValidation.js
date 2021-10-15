08d84594c16930aee4e450c420349c35b6770c22
Anis Brachemi <anisbrachemi@hotmail.com>
2021-10-15 18:59:10 -0400

    created node server

--- server/src/ai/utils/schemaValidation.js
+++ server/src/ai/utils/schemaValidation.js
@@ -3,7 +3,7 @@ const Joi = require('@hapi/joi');
 const schemas = {
     valid_link: Joi.object().keys({
         url: Joi.string().required(),
-    }),
+    }).unknown(true),
 };
 
 module.exports = schemas;
