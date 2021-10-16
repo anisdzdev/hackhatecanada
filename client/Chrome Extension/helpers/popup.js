d478321d378a05504c9dc0f8106e268100aa55b1
jaspreet305 <khanda987@gmail.com>
2021-10-16 17:14:47 -0400

    fixed report and added back btn

--- client/Chrome Extension/helpers/popup.js
+++ client/Chrome Extension/helpers/popup.js
@@ -8,7 +8,10 @@ const show_popup = function (body) {
                 <div>
                       <p><strong>Hate Speech Scanner</strong> has flagged this website as potentially containing hate speech.
                          You may click off the website or continue by clicking on the button below.</p>
-                    <button class="hcc-md-close" id="hcc-close-btn">Proceed anyway</button>
+                    <div class="hcc-popup-buttons">   
+                        <button onclick="history.back()" class="hcc-md-back">Take me back</button>
+                        <button class="hcc-md-close" id="hcc-close-btn">Proceed anyway</button>
+                    </div>
                 </div>
             </div>
         </div>
